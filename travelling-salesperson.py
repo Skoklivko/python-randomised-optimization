a7974aa28cc0dd05b7c92652935c98b1f6658739
Angel Tushev <angel.p.tushev96@gmail.comgit config --global user.name Angel>
2022-07-10 16:20:52 +0300

    Solving the traveling salesperson problem using mlrose

---
+++ travelling-salesperson.py
@@ -0,0 +1,27 @@
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import mlrose
+
+
+coords_list = [(1, 1), (4, 2), (5, 2), (6, 4), (4, 4), (3, 6), (1, 5), (2, 3)]
+fitness_coords = mlrose.TravellingSales(coords = coords_list)
+
+# list of distances between pairs of cities
+dist_list = [(0, 1, 3.1623), (0, 2, 4.1231), (0, 3, 5.8310), (0, 4, 4.2426), \
+             (0, 5, 5.3852), (0, 6, 4.0000), (0, 7, 2.2361), (1, 2, 1.0000), \
+             (1, 3, 2.8284), (1, 4, 2.0000), (1, 5, 4.1231), (1, 6, 4.2426), \
+             (1, 7, 2.2361), (2, 3, 2.2361), (2, 4, 2.2361), (2, 5, 4.4721), \
+             (2, 6, 5.0000), (2, 7, 3.1623), (3, 4, 2.0000), (3, 5, 3.6056), \
+             (3, 6, 5.0990), (3, 7, 4.1231), (4, 5, 2.2361), (4, 6, 3.1623), \
+             (4, 7, 2.2361), (5, 6, 2.2361), (5, 7, 3.1623), (6, 7, 2.2361)]
+
+fitness_dist = mlrose.TravellingSales(distances = dist_list)
+
+problem_fit = mlrose.TSPOpt(length=8, fitness_fn=fitness_coords,
+                            maximize=False)
+
+best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state=2)
+
+print('the best state found is: ', best_state)
+print ('the fitness at the best state is: ', best_fitness)
